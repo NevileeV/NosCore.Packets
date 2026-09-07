945687fe3bc1963bbfb9d54b67cbf47938174f86
NevileeV <dami1-79@wp.pl>
2026-09-07 18:54:50 +0200

    Fix specialist point packet layout

--- src/NosCore.Packets/ClientPackets/Specialists/SpTransformPacket.cs
+++ src/NosCore.Packets/ClientPackets/Specialists/SpTransformPacket.cs
@@ -19,15 +19,21 @@ namespace NosCore.Packets.ClientPackets.Specialists
         public int? TransportId { get; set; }
 
         [PacketIndex(2, IsOptional = true)]
-        public short? SpecialistDamage { get; set; }
+        public short? Unknown2 { get; set; }
 
         [PacketIndex(3, IsOptional = true)]
-        public short? SpecialistDefense { get; set; }
+        public short? Unknown3 { get; set; }
 
         [PacketIndex(4, IsOptional = true)]
-        public short? SpecialistElement { get; set; }
+        public short? SpecialistDamage { get; set; }
 
         [PacketIndex(5, IsOptional = true)]
+        public short? SpecialistDefense { get; set; }
+
+        [PacketIndex(6, IsOptional = true)]
+        public short? SpecialistElement { get; set; }
+
+        [PacketIndex(7, IsOptional = true)]
         public short? SpecialistHP { get; set; }
     }
 }
